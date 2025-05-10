834ef35156c6c08ef64a4a323bceba19f0358bca
theMungai <mungaithesimon@gmail.com>
2025-05-11 02:20:28 +0300

    Separates ServicePage and CustomerFeedBack components for readability

--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -3,26 +3,8 @@ import Layout from '../components/Layout'
 import ServiceHero from '../components/ServiceHero'
 import FindDoctor from '../components/common/FindDoctor'
 import Services from '../components/landing/Services'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStar } from '@fortawesome/free-solid-svg-icons'
-import { faStarHalfStroke } from '@fortawesome/free-regular-svg-icons'
-
-function CustomerFeedBack({image}){
-  return(
-    <div className = "p-7 border border-[#cfcfcf] font-montserrat w-[30%]">
-      <div className ="flex items-center text-[#F3CD03] mb-5">
-        <FontAwesomeIcon icon={faStar}/>
-        <FontAwesomeIcon icon={faStar}/>
-        <FontAwesomeIcon icon={faStar}/>
-        <FontAwesomeIcon icon={faStar}/>
-        <FontAwesomeIcon icon={faStarHalfStroke}/>
-      </div>
-      <p className = "text-[#737373] text-[13px] leading-6 my-5 ">I was incredibly nervous before my surgery, but the doctors and nurses made me feel completely at ease. Their professionalism, empathy, and constant reassurance turned a stressful situation into one I could handle with confidence. I’m truly grateful for the outstanding care I received.</p>
-
-      <img src={`src/assets/images/customer-${image}.jpg`} alt="Customer feedback" className = "w-[45px] h-[45px] rounded-full"/>
-    </div>
-  )
-}
+import FAQ from '../components/FAQ'
+import CustomerFeedBacks from '../components/CustomerFeedBack'
 
 const ServicesPage = () => {
   return (
@@ -31,17 +13,7 @@ const ServicesPage = () => {
         <ServiceHero/>
         <FindDoctor/>
         <Services/>
-
-        <div className = "font-montserrat px-[130px]">
-          <h1 className = "text-customTealBlue text-[36px] font-extrabold text-center mb-2.5">what our customers say</h1>
-          <p className ="text-[#555555] text-[16px] text-center mb-[60px] mx-auto w-[60%]">Our patients consistently praise the care, compassion, and professionalism they receive at our hospital. From the moment they walk in, they feel heard, supported, and treated with the utmost respect. Whether it’s a routine visit or a life-saving procedure, we strive to deliver exceptional healthcare experiences every single time.</p>
-
-          <div className="flex justify-between items-center" >
-            <CustomerFeedBack image={1}/>
-            <CustomerFeedBack image={2}/>
-            <CustomerFeedBack image={3}/>
-          </div>
-        </div>
+        <CustomerFeedBacks/>
       </Layout>
     </div>
   )
