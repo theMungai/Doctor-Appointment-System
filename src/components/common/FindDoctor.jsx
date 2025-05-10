c645fc150fe9b9111c2809da55f1933965bca61a
theMungai <mungaithesimon@gmail.com>
2025-05-10 13:32:46 +0300

    Add FindDoctor component

---
+++ src/components/common/FindDoctor.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const FindDoctor = () => {
+  return (
+    <div className="find-a-doctor-container mb-[150px] ">
+        <h1 className='text-[32px] text-black font-dmsans font-bold mb-8'>Find A Doctor</h1>
+        <form className="flex flex-wrap items-center gap-4 justify-between p-4 bg-white rounded-lg">
+          <div className='flex gap-x-10 items-center'>
+            <input type="text" placeholder="Name" required className="flex-1 min-w-[150px] w-[360px] p-4 outline-none border border-customTealBlue rounded-[8px] [8px] placeholder:text-[16px] text-[#555555] font-light"/>
+
+            <input type="text" placeholder="Speciality" required className="flex-1 min-w-[150px] w-[360px] p-4 outline-none border border-customTealBlue rounded-[8px] [8px] placeholder:text-[16px] text-[#555555] font-light"/>
+          </div>
+
+          <label className="flex items-center gap-x-6">
+            <span className="text-[20px] font-medium text-gray-700">Available</span>
+            <div className="relative inline-flex items-center cursor-pointer">
+              <input type="checkbox" className="sr-only peer" />
+              <div className="w-16 h-6 bg-white border border-gray-400 rounded-full peer-checked:bg-white peer-checked:border-customTealBlue transition duration-300"></div>
+              <div className="absolute left-1 top-1 w-4 h-4 bg-gray-400 rounded-full transition-transform duration-300 peer-checked:translate-x-10 peer-checked:bg-customTealBlue "></div>
+            </div>
+          </label>
+
+          <button className="bg-customTealBlue text-white font-dmsans text-[1.25rem] rounded-[8px] py-[20px] px-[80px]">
+            Search
+          </button>
+        </form>
+
+      </div>
+  )
+}
+
+export default FindDoctor
--- src/components/landing/Hero.jsx
+++ src/components/landing/Hero.jsx
@@ -4,6 +4,7 @@ import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
 import Services from './Services';
 import TeamMembers from './TeamMembers';
 import Testimonials from './Testimonials';
+import FindDoctor from '../common/FindDoctor';
 
 function Professionals({image}){
   return(
@@ -76,30 +77,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className="find-a-doctor-container mb-[150px] ">
-        <h1 className='text-[32px] text-black font-dmsans font-bold mb-8'>Find A Doctor</h1>
-        <form className="flex flex-wrap items-center gap-4 justify-between p-4 bg-white rounded-lg">
-          <div className='flex gap-x-10 items-center'>
-            <input type="text" placeholder="Name" required className="flex-1 min-w-[150px] w-[360px] p-4 outline-none border border-customTealBlue rounded-[8px] [8px] placeholder:text-[16px] text-[#555555] font-light"/>
-
-            <input type="text" placeholder="Speciality" required className="flex-1 min-w-[150px] w-[360px] p-4 outline-none border border-customTealBlue rounded-[8px] [8px] placeholder:text-[16px] text-[#555555] font-light"/>
-          </div>
-
-          <label className="flex items-center gap-x-6">
-            <span className="text-[20px] font-medium text-gray-700">Available</span>
-            <div className="relative inline-flex items-center cursor-pointer">
-              <input type="checkbox" className="sr-only peer" />
-              <div className="w-16 h-6 bg-white border border-gray-400 rounded-full peer-checked:bg-white peer-checked:border-customTealBlue transition duration-300"></div>
-              <div className="absolute left-1 top-1 w-4 h-4 bg-gray-400 rounded-full transition-transform duration-300 peer-checked:translate-x-10 peer-checked:bg-customTealBlue "></div>
-            </div>
-          </label>
-
-          <button className="bg-customTealBlue text-white font-dmsans text-[1.25rem] rounded-[8px] py-[20px] px-[80px]">
-            Search
-          </button>
-        </form>
-
-      </div>
+      <FindDoctor/>
 
       <div className="results-numbers-container">
         <h1 className='text-center text-[32px] mb-[70px] font-dmsans font-bold text-customTealBlue'>Our results in numbers</h1>
